278f87f0e8f056c99c6cdbc5ad9e83fea990ec1f
Oskar <oskarkuchta5@gmail.com>
2024-03-14 21:45:34 +0100

    Add post to API

--- customers/src/app.js
+++ customers/src/app.js
@@ -1,16 +1,26 @@
 const { app, port, customers } = require("./utils");
+const express = require("express");
 
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
 app.get("/api/customers", (req, res) => {
+    const customerData = customers.map((customer) => ({
+        name: customer.name,
+        industry: customer.industry
+    }));
 
-    const htmlResponse = customers.map((customer) => `
-        <div>
-            <h2>Customer name: ${customer.name}</h2>
-            <p>Industry: ${customer.industry}</p>
-        </div>
-    `).join("");
-    res.send(htmlResponse);
+    res.send({
+        data: customerData
+    });
 });
 
+
+app.post("/api/customers", (req, res) => {
+    console.log(req.body)
+    res.send(req.body)
+})
 app.listen(port, () => {
     console.log(`Server running on port ${port}...`)
 })
